297d8e47bfde345a8b30d83f599ead5347a093e5
u239105 <anna.emmenegger2@sbb.ch>
2023-04-04 15:56:04 +0200

    renamed variable

--- src/org/java/annalostincode/DiffieHellmanEncryptor.java
+++ src/org/java/annalostincode/DiffieHellmanEncryptor.java
@@ -15,9 +15,9 @@ public class DiffieHellmanEncryptor {
     }
 
     public static boolean setSecret(DiffieHellmanEncryptor k1, DiffieHellmanEncryptor k2, BigInteger modulo, BigInteger base) {
-        BigInteger k1result = k1.compute(base, modulo);
-        BigInteger k2result = k2.compute(base, modulo);
-        return Objects.equals(k1.compute(k2result, modulo), k2.compute(k1result, modulo));
+        BigInteger k1appliedPrivateKey = k1.compute(base, modulo);
+        BigInteger k2appliedPrivateKey = k2.compute(base, modulo);
+        return Objects.equals(k1.compute(k2appliedPrivateKey, modulo), k2.compute(k1appliedPrivateKey, modulo));
     }
 
 }
